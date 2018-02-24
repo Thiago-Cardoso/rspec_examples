73ac2b136c9725d9a42f3171e9a207115d58b69b
Thiago-Cardoso <trovador182@gmail.com>
2018-02-24 14:47:55 -0300

    factory_bot

--- test_app/spec/factories/customer.rb
+++ test_app/spec/factories/customer.rb
@@ -1,7 +1,36 @@
 # /spec/factories/customer.rb
 FactoryBot.define do
     factory :customer do
-        name "Thiago Cardoso"
-        email "thiagocardosooti@gmail.com"
+
+        transient do 
+            upcased false
+        end
+
+        name { Faker::Name.name }
+        email { Faker::Internet.email }
+        #sequence(:email) { |n| "meu_email-#{n}@email.com"}
+        gender ['M', 'F'].sample
+
+        trait :male do
+           gender 'M'
+        end
+
+        trait :female do
+            gender 'F'
+         end
+    
+        factory :customer_vip do
+            vip true
+            days_to_pay 30
+        end
+
+        factory :customer_default do
+            vip false
+            days_to_pay 15
+        end
+
+        after(:create) do |customer, evaluator|
+            customer.name.upcase! if evaluator.upcased
+        end
     end
 end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -2,14 +2,48 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
 
-  fixtures:customers
-
-  it 'Create a Customer' do
-     # subject.name = 'Thiago'
-     # subject.email = 'thiagocardosooti@gmail.com'
-     #subject.save
-      customer = customers(:thiago)
-      #expect(subject.full_name).to eq("Sr. Thiago")
-      expect(customer.full_name).to eq("Sr. Thiago")
+  it '#full_name' do
+    customer = create(:customer)
+    puts customer.email
+    expect(customer.full_name).to start_with("Sr. ")
   end
+
+  it '#full_name - Sobrescrevendo Atributo' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
+  end
+
+  it 'Herança' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
+  end
+
+  it 'Usando o attributes_for' do
+    attrs = attributes_for(:customer)
+    attrs1 = attributes_for(:customer_vip)
+    attrs2 = attributes_for(:customer_default)
+    #puts (attrs)
+    #puts (attrs1)
+    #puts (attrs2)
+  end
+
+
+  it 'Usando o attributes_for com expect' do
+    attrs = attributes_for(:customer)
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to start_with("Sr. ")
+  end
+
+
+  it 'Atributo transitorio' do
+    customer = create(:customer_default, upcased: true)
+    expect(customer.name.upcase).to eq(customer.name)
+  end
+
+  it 'Cliente Masculino' do
+    customer = create(:customer_vip, :male)
+    expect(customer.gender).to eq('M')
+  end
+
+  #it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 end
